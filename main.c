1aed46235f5749f52311bcfe50f7815a6caa4a9d
Luke Burton <lukesburton19@gmail.com>
2024-03-25 22:13:44 -0600

    update main.c to include logging requests and requiring a command-line arg to specify the port

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netinet/tcp.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <arpa/inet.h>
 
 int main(int argc, char* argv[])
 {
-    int port;
     int backlog_len = 128;
     int buffer_size = 1024;
     char req_buffer[buffer_size];
@@ -18,24 +17,13 @@ int main(int argc, char* argv[])
     "Content-type: text/html\r\n\r\n"
     "<html>Hello, World!</html>\r\n";
 
-    if (argc < 2)
+    if (argc != 2 || atoi(argv[1]) == 0)
     {
-        printf("No port specified. Default is 8080\n");
-        port = 8080;
+        printf("Please specify a non-zero integer port.\n");
+        return 1;
     }
 
-    else 
-    {
-        port = atoi(argv[1]);
-    }
-
-    // handle port set to 0 (such as when a string is passed to args)
-    if (port == 0) 
-    {
-        printf("No port specified. Default is 8080\n");
-        port = 8080;
-    }
-
+    int port = atoi(argv[1]);
 
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_fd < 0)
@@ -48,9 +36,12 @@ int main(int argc, char* argv[])
     host_addr.sin_family = AF_INET;
     host_addr.sin_port = htons(port);
     host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    int len_hostaddr = sizeof(host_addr);
+    int host_addr_length = sizeof(host_addr);
 
-    if (bind(socket_fd, (struct sockaddr *)&host_addr, len_hostaddr) < 0)
+    struct sockaddr_in client_addr;
+    int client_addr_length = sizeof(client_addr);
+
+    if (bind(socket_fd, (struct sockaddr *)&host_addr, host_addr_length) < 0)
     {
         perror("webs failed to bind socket\n");
         return 1;
@@ -65,14 +56,19 @@ int main(int argc, char* argv[])
 
     for(;;)
     {
-        int conn_fd = accept(socket_fd, (struct sockaddr *)&host_addr, &len_hostaddr);
+        int conn_fd = accept(socket_fd, (struct sockaddr *)&host_addr, &host_addr_length);
         if (conn_fd < 0) 
         { 
             perror("webs detected a request and failed to accept"); 
             continue;
         }
 
-        printf("webs accepted a request");
+        int socket_name = getsockname(conn_fd, (struct sockaddr *)&client_addr, &client_addr_length);
+        if (socket_name < 0)
+        {
+            perror("webs could not retrieve client socket name");
+            continue;
+        }
 
         int read_req = read(conn_fd, req_buffer, buffer_size);
         if (read_req < 0)
@@ -80,7 +76,11 @@ int main(int argc, char* argv[])
             perror("webs could not read request");
             continue;
         }
-
+        char method[buffer_size], uri[buffer_size], version[buffer_size];
+        sscanf(req_buffer, "%s %s %s", method, uri, version);
+        printf("[%s:%u] %s %s %s\n", inet_ntoa(client_addr.sin_addr),
+               ntohs(client_addr.sin_port), method, version, uri);
+        
         int write_res = write(conn_fd, response, strlen(response));
 
         close(conn_fd); 
