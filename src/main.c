8e1ba4a3723c572fb7e52a3ae7f504bd855e015e
Luke Burton <lukesburton19@gmail.com>
2024-07-11 17:01:46 -0600

    use new network wrapper functions and structs

--- src/main.c
+++ src/main.c
@@ -18,75 +18,86 @@ void handle_signal(int signal) {
     stop = 1;
 }
 
-void handle_connection(AppContext *context, Session *session)
-{
-    if (parse_request(session) < 0)
-    {
-        return;
-    }
-
-    printf("%s %s %s\n", session->method, session->path, session->protocol);
-    printf(session->full_request);
-
-    char *response = (char *)malloc(MAX_RESPONSE_SIZE * sizeof(char));
-    if (response == NULL)
-    {
-        fprintf(stderr, "Failed to allocate memory for request\n");
-        close(session->clientfd);
-        delete_session(session);
-        return;
-    }
-
-    get_response(response, context, session);
-
-    if (send(session->clientfd, response, 1024 - 1, 0) < 0)
-    {
-        printf("send error: %s\n", strerror(errno));
-        free(response);
-    }
-
-    free(response);
-    close(session->clientfd);
-    delete_session(session);
-}
-
 int main(int argc, char* argv[])
 {
-    struct sigaction sa;
-    sa.sa_handler = handle_signal;
-    sa.sa_flags = 0;
-    sigemptyset(&sa.sa_mask);
-    if (sigaction(SIGINT, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
+    struct sigaction act;
+    act.sa_handler = handle_signal;
+    sigaction(SIGINT, &act, NULL);
+
+    struct config config = create_config(argc, argv);
+
+    struct addrinfo *info = get_info(config.port);
+    if (!info)
+    {
+        printf("Addrinfo error\n");
     }
 
-    AppContext context = configure_context(argc, argv);
-
-    if (begin_listen(&context) < 0)
+    int sockfd = get_socket(info);
+    if (sockfd < 0)
     {
-        printf("begin_listen error\n");
+        printf("Socket error\n");
+    }
+
+    if (bind_socket(sockfd, info) < 0)
+    {
+        printf("Bind error\n");
+    }
+
+    if (listen_on_socket(sockfd, 10) < 0)
+    {
+        printf("Listen error\n");
         return -1;
     }
 
-    printf("nGate is listening on port %s\n", context.port);
+    printf("nGate is listening on port %s\n", config.port);
 
     while (!stop)
     {
-        Session *session = create_session();
-        if (accept_connection(&context, session) < 0)
+        struct session *session = create_session();
+        struct sockaddr_storage clientaddr;
+        socklen_t addrlen = sizeof(clientaddr);
+
+        session->clientfd = accept(sockfd, (struct sockaddr *)&clientaddr, &addrlen);
+        if ((session->clientfd < 0))
         {
-            if (stop) 
-            {
-                break;
-            }
+            delete_session(session);
+            printf("accept error: %s\n", strerror(errno));
             continue;
         }
 
-        handle_connection(&context, session);
+        if (parse_request(session) < 0)
+        {
+            delete_session(session);
+            printf("Parse request error\n");
+            continue;
+        }
+
+        printf("%s %s %s\n", session->method, session->path, session->protocol);
+        // printf("%s\n", session->full_request);
+
+        char *response = (char *)malloc(MAX_RESPONSE_SIZE * sizeof(char));
+        if (response == NULL)
+        {
+            fprintf(stderr, "Malloc response error\n");
+            close(session->clientfd);
+            delete_session(session);
+            continue;
+        }
+
+        get_response(response);
+
+        if (send(session->clientfd, response, 1024 - 1, 0) < 0)
+        {
+            printf("send error: %s\n", strerror(errno));
+            free(response);
+        }
+
+        free(response);
+        close(session->clientfd);
+        delete_session(session);
     }
 
-    close(context.sockfd);
+    close(sockfd);
 
     return 0;
 }
