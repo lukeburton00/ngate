855ca9a9fea92e91c2473629c89221d3bc90b809
Luke Burton <lukesburton19@gmail.com>
2024-07-13 18:41:25 -0600

    add building config from file

--- include/config.h
+++ include/config.h
@@ -2,7 +2,9 @@
 
 struct config
 {
-    char *port;
+    char port[6];
+    char *filepath;
 };
 
 struct config create_config(int argc, char **argv);
+int is_valid_port(const char *port);
--- src/config.c
+++ src/config.c
@@ -1,35 +1,120 @@
 #include "../include/config.h"
 
+#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+struct config_option
+{
+    char key[128];
+    char value[128];
+};
+
+static const char *usage = ("Usage: ngate (args)\n"
+       "-f [path] - define path to configuration file\n"
+       "-h - display options\n");
+
 struct config create_config(int argc, char **argv)
 {
-    char *default_port = "3000";
+    if (argc < 2)
+    {
+        printf("%s", usage);
+        exit(1);
+    }
 
-    struct config config;
-    config.port = NULL;
+    struct config config = {.filepath = NULL};
 
     for (int i = 1; i < argc; i++)
     {
-        if ((strcmp(argv[i], "-p") == 0) && i + 1 < argc)
+        if ((strcmp(argv[i], "-f") == 0) && i + 1 < argc)
         {
-            config.port = argv[i + 1];
+            config.filepath = argv[i + 1];
+            i++;
         }
 
-        if ((strcmp(argv[i], "-h") == 0))
+        else if ((strcmp(argv[i], "-h") == 0))
         {
-            printf("Usage: %s [-p port] [-c config_file] [-h]\n", argv[0]);
+            printf("%s", usage);
             exit(0);
+        }
+
+        else
+        {
+            printf("Ignoring unrecognized parameter %s\n", argv[i]);
         }
     }
 
-    if (config.port == NULL)
+    if (!config.filepath)
     {
-        printf("No port specified. Default is %s\n", default_port);
-        config.port = default_port;
+        printf("No configuration file provided. %s", usage);
+        exit(1);
+    }
+
+    FILE *config_file = fopen(config.filepath, "r");
+    if (!config_file)
+    {
+        printf("Failed to open config file\n");
+        exit(1);
+    }
+
+    struct config_option *option = calloc(1, sizeof(struct config_option));
+    if (!option)
+    {
+        printf("Calloc error");
+        exit(1);
+    }
+
+    while(fscanf(config_file, "%s = %s", option->key, option->value) == 2)
+    {
+        if (strcmp(option->key, "port") == 0)
+        {
+            strncpy(config.port, option->value, sizeof(config.port) - 1);
+            config.port[sizeof(config.port) - 1] = '\0';
+            continue;
+        }
+
+        if (strcmp(option->key, "#") == 0)
+        {
+            while (fgetc(config_file) != '\n'){}
+            continue;
+        }
+    }
+
+    fclose(config_file);
+    free(option);
+
+    if (!is_valid_port(config.port))
+    {
+        printf("Parameter port was not defined or is invalid\n");
+        exit(1);
     }
 
     return config;
+}
+
+int is_valid_port(const char *port)
+{
+    if (port == NULL)
+    {
+        return 0;
+    }
+
+    for (int i = 0; port[i] != '\0'; i++)
+    {
+        if (!isdigit(port[i]))
+        {
+            return 0;
+        }
+    }
+
+    int port_num = atoi(port);
+    if (port_num >= 0 && port_num <= 65535)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
